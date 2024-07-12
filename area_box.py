8c9f4d99024517a457b597b50899aa8794dc5195
elakkiya0710 <elakkiya0710@gmail.com>
2024-07-12 14:54:07 +0530

    Add files via upload

---
+++ area_box.py
@@ -0,0 +1,26 @@
+import math
+
+def circle(radius):
+    """
+    To find the area of the Circle
+    circle(radius)
+    radius - enter the radius of the circle
+    """
+
+    area = math.pi * radius ** 2
+    return area
+
+
+def rectangle(length, breadth):
+    """
+    To find the area of the Rectangle
+    rectangle(length, breadth)
+    length - enter the length of the rectangle
+    breadth - enter the breadth of the rectangle
+    """
+
+    area = length * breadth
+    return area
+
+
+
